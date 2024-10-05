fefef7f214b93d3421e794f12c41b41a35d601fa
mtantaoui <mohamedalmahdi.tantaoui@gmail.com>
2024-10-05 13:00:38 +0200

    remove derivative evaluation from OrthoPoly trait

--- src/utils/orthogonal_polynomials.rs
+++ src/utils/orthogonal_polynomials.rs
@@ -1,5 +1,3 @@
-// extern crate test;
-
 use std::{fmt::Debug, ops::AddAssign};
 
 use num::Float;
@@ -8,8 +6,6 @@ pub trait OrthogonalPolynomial<F: Float + Debug + AddAssign> {
     fn new(degree: usize) -> Self;
 
     fn eval(&self, x: F) -> F;
-
-    // fn eval_derivative(&self, x: F) -> F;
 
     fn zeros(&self) -> Vec<F>;
 }
