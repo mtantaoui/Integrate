8d931f9f93a6b9db8bc22e1073639e73b3a0456a
mtantaoui <mohamedalmahdi.tantaoui@gmail.com>
2024-12-01 22:23:28 +0100

    a small comment

--- src/romberg.rs
+++ src/romberg.rs
@@ -204,6 +204,8 @@ where
         })
         .collect_into_vec(&mut trapezoidals);
 
+
+        // Storing computed values (shared between threads)
     let cache: Mutex<HashMap<(U, U), F2>> = Mutex::new(HashMap::new());
 
     let integral = romberg(
